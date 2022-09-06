21dad5a0e493b0add7aa4c5f8a7816c44c64f4cb
Gurutejetha <112626821+Gurutejetha@users.noreply.github.com>
2022-09-06 09:39:37 +0530

    Add files via upload

---
+++ student.test.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+class student
+{
+	private:
+		char  name[30];
+		int   rollNo;
+		int   total;
+		float avg;
+	public:
+		void getDetails(void);
+		void putDetails(void);
+};
+void student::getDetails(void){
+	cout << "Enter name: " ;
+	cin >> name;
+	cout << "Enter roll number: ";
+	cin >> rollNo;
+	cout << "Enter total marks outof 500: ";
+	cin >> total;
+	avg=(float)total/500*100;
+}
+void student::putDetails(void){
+	cout << "Student details:\n";
+	cout << "Name:"<< name << ",Roll Number:" << rollNo << ",Total:" << total << ",average" << avg;
+}
+int main()
+{
+	student std;	
+	std.getDetails();
+	std.putDetails();
+	return 0;
+}
